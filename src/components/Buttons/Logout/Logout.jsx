3049d65c8b706d8426d3add1c8d065f938c7461a
RAMarenco <00080121@uca.edu.sv>
2023-06-11 17:16:53 -0600

    Add: Feature/WebPage-0012/Header

---
+++ src/components/Buttons/Logout/Logout.jsx
@@ -0,0 +1,9 @@
+import classes from './Logout.module.scss';
+
+export const Logout = () => {
+    return (
+        <button className={ classes['Logout'] }>
+            Logout
+        </button>
+    )
+}
