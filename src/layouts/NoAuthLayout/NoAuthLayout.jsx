0dd66e682830f530f08750ffe77a9a7d9863801b
RAMarenco <00080121@uca.edu.sv>
2023-06-22 00:18:48 -0600

    Add: Feature/WebPage-2263/Landing Page

--- src/layouts/NoAuthLayout/NoAuthLayout.jsx
+++ src/layouts/NoAuthLayout/NoAuthLayout.jsx
@@ -1,10 +1,17 @@
+import classes from './NoAuthLayout.module.scss';
+import logo from './../../assets/logo.png';
 import { Outlet } from "react-router-dom";
 
-export const NoAuthLayout = () => {
+export const NoAuthLayout = ({scrollTop}) => {
+    console.log(scrollTop);
     return (
         <>
-            <header></header>
-            <main>
+            <header className={ scrollTop >= 25 ? [classes["Header"], classes["Scrolled"]].join(" ") : classes["Header"] }>
+                <div className={ classes["Logo"] }>
+                    <img src={logo}/> <h1>MINGO</h1>
+                </div>                
+            </header>
+            <main className={ classes["Main"] }>
                 <Outlet/>
             </main>
         </>
